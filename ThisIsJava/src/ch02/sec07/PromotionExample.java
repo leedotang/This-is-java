8d6172819b96a6722b9c6ea373a00302269d2b9d
fasfaff <fasfaff@naver.com>
2023-07-09 18:18:15 +0900

    이것이 자바다 2.7 자동 타입변환

---
+++ ThisIsJava/src/ch02/sec07/PromotionExample.java
@@ -0,0 +1,33 @@
+package ch02.sec07;
+
+public class PromotionExample {
+
+	public static void main(String[] args) {
+
+		// 기본타입 허용 범위 byte < short, char < int < long < float < double
+		
+		// 자동타입 변환
+		byte byteValue = 10;
+		int intValue = byteValue;
+		System.out.println("intValue: " + intValue);
+		System.out.println("=======================");
+		
+		char charValue = '가';
+		intValue = charValue;
+		System.out.println("가의 유니코드: " + intValue);
+		System.out.println("=======================");
+		
+		intValue = 50;
+		long longValue = intValue;
+		System.out.println("longValue: " + intValue);
+		System.out.println("========================");
+		
+		longValue = 100;
+		float floatValue = longValue;
+		System.out.println("floatValue: " + longValue);
+		
+		
+		
+	}
+
+}
