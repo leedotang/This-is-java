978a6a0a100e9616b40e16f9a7b24b5c3a9753c0
fasfaff <fasfaff@naver.com>
2023-07-09 17:10:03 +0900

    이것이 자바다 2.1 변수 선언

---
+++ ThisIsJava/src/ch02/sec01/VariableExchangeExample.java
@@ -0,0 +1,13 @@
+package ch02.sec01;
+
+public class VariableExchangeExample {
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 5;
+		System.out.println("X: " + x + ", Y: " + y);
+		
+		int temp = x;
+		x = y;
+		System.out.println("X: " + x + ", Y: " + y);
+	}
+}
