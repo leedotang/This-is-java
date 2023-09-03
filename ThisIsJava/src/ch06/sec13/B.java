9c07386c703ae50390a0f65fd53da76c532f4b42
fasfaff <fasfaff@naver.com>
2023-09-03 18:43:08 +0900

    이것이 자바다 6.13 접근 제한자

---
+++ ThisIsJava/src/ch06/sec13/B.java
@@ -0,0 +1,10 @@
+package ch06.sec13;
+
+public class B {
+	
+//	A a;
+	A a1 = new A(true);
+	A a2 = new A(1);
+//	A a3 = new A("문자열");	// private 때문에 에러
+	
+}
---
+++ ThisIsJava/src/ch06/sec13/exam01/C.java
@@ -0,0 +1,14 @@
+package ch06.sec13.exam01;
+
+import ch06.sec13.A;
+import ch06.sec13.B;
+
+public class C {
+	
+//	A a;
+	B b;
+	
+	A a1 = new A(true);
+//	A a2 = new A(1); default
+//	A a3 = new A("문자열"); private
+}
